6b6c2ad8b9fcfb6a057d84a5db75a1a3b50ed970
Aleksandra Kalerina <aleksandra.bm@mail.ru>
2024-06-11 16:27:11 +0300

    Json Schema

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,14 +1,18 @@
 package ru.netology.rest;
 
+import org.hamcrest.Matcher;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 
 class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
         // Given - When - Then
         // Предусловия
+
         given()
                 .baseUri("http://localhost:9999/api/v1")
                 // Выполняемые действия
@@ -16,6 +20,14 @@ class MobileBankApiTestV1 {
                 .get("/demo/accounts")
                 // Проверки
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                .body("",hasSize(3))
+                .body("[0].id",equalTo(1));
+
+
+
+
+
+
     }
 }
